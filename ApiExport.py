9c36a60ae233c3937082ba50380be2587f42c2be
rayan <tiray972@gmail.com>
2022-03-18 10:02:52 -0400

    modifié :         ApiExport.py

--- ApiExport.py
+++ ApiExport.py
@@ -1,2 +1,9 @@
+import requests
 import time
-time.time
+
+tableauexemple='/resultats?debut=18-03-2019&fin=18-03-2022'
+
+reponce=requests.get('http://www.naiades.eaufrance.fr/acces-donnees#/physicochimie')
+print('\n')
+print(reponce.status_code)
+print(reponce)
