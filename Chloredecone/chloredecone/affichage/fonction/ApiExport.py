3d95e70ca4d4afc4ae7dd2b5a4a2a71d02de26b2
unknown <tiray972@gmail.com>
2022-04-21 19:08:17 -0400

    test des lien sur map

--- Chloredecone/chloredecone/affichage/fonction/ApiExport.py
+++ Chloredecone/chloredecone/affichage/fonction/ApiExport.py
@@ -98,13 +98,14 @@ parr={'service':'SANDRE:Metadata',
 # s=requests.get(httpjson,dico) ,
 # print(s.text)
 # l=donnejson(httpjson,dico),,'parameter':'1301'
+'''#utiliser plus tard
 j=json.loads(requests.get(creationDurlHubeau()).text)
 print(j['data'][0]['libelle_station'],end='')
 print(' |prelevé le :',j['data'][0]['date_prelevement'])
 print(j['data'][0]['code_unite'],j['data'][0]['symbole_unite'])
 print(' libelle parametre :',j['data'][0]['libelle_parametre'])
-
+'''
 
 # print(j['data'][0].keys())
 # print(CreationUrlAdes('1186ZZ0185/P'))
-print(creationDurlHubeau())
+# print(creationDurlHubeau())
--- Chloredecone/chloredecone/affichage/fonction/carte.py
+++ Chloredecone/chloredecone/affichage/fonction/carte.py
@@ -3,19 +3,23 @@ import folium
 from folium import plugins
 import pandas as pd
 import requests
+from affichage.fonction.ApiExport import creationDurlHubeau
 def mapmaxmin():
     requette=requests.get("https://hubeau.eaufrance.fr/api/v1/qualite_rivieres/station_pc?code_departement=972")
     new=pd.DataFrame(json.loads(requette.text)['data'])
+    new['lien']='<a href='+str(creationDurlHubeau(code_station=new['code_station']))+'>'+str(new['libelle_station'])+'</a>'
+
     # print(new['longitude', 'latitude'])'longitude', 'latitude'
     map_enum_icons = folium.Map([14.6, -61], zoom_start=11)
     for i in new.itertuples():
         folium.Marker(location=[i.latitude, i.longitude],
-                    popup=i.libelle_station,
+                    popup=i.lien,#libelle_station
                     icon=plugins.BeautifyIcon(number=10,
                                                 border_color='blue',
                                                 border_width=1,
                                                 text_color='red',
                                                 inner_icon_style='margin-top:0px;')).add_to(map_enum_icons)
     #     number reste a changer mtre des valeur
-    map_enum_icons.save('./affichage/templates/affichage/map.html')
+    # print(new.lien)
     return map_enum_icons._repr_html_()
+mapmaxmin()
